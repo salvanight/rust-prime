5d214a0cb7b4f49022cb96e3ae9f402c72b15cc4
salvanight <zero.simentech@gmail.com>
2025-06-03 16:24:58 -0600

    Refactor tests and tokenizer setup

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,16 @@ use std::io::Read; // For load_pre_tokenized_from_json
 // We will add functions and structures here later.
 
 pub mod config;
-pub mod common;
-pub mod accelerator;
-pub mod attention;
-pub mod mlp;
-pub mod model;
-pub mod tokenizer; // This will now load src/tokenizer.rs
 
-// Remove the gpt2_tokenizer_tests module from here, as tests should be in their respective files.
-// The tests for TokenizerWrapper are in src/tokenizer.rs.
-// The tests I previously added for a GPT2Tokenizer in *this* file (lib.rs) were misplaced
-// if the intention was to test TokenizerWrapper from src/tokenizer.rs.
+#[cfg(feature = "ndarray_backend")]
+pub mod common;
+#[cfg(feature = "ndarray_backend")]
+pub mod accelerator;
+#[cfg(feature = "ndarray_backend")]
+pub mod attention;
+#[cfg(feature = "ndarray_backend")]
+pub mod mlp;
+#[cfg(feature = "ndarray_backend")]
+pub mod model;
+
+pub mod tokenizer; // This will now load src/tokenizer.rs
